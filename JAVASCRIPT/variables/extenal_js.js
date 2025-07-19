bdc1c2475b2c9c8450d1f1f2ada52f9a2f5e8b3f
githubsneha123 <123347614+githubsneha123@users.noreply.github.com>
2025-07-19 17:22:02 +0530

    Add files via upload

---
+++ JAVASCRIPT/variables/extenal_js.js
@@ -0,0 +1,13 @@
+function main()
+{
+    let n=10;
+    console.log("the value of n:"+n);
+    n="sneha";
+    console.log(n);
+    n=false;
+    console.log(n);
+    n=[1,2,3,4,5];
+    console.log(n);
+    console.log("the value of n:"+n);
+}
+main();
